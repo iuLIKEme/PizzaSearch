b888505d7616051b6c4a0eac664ddd99f6ac3f8a
djmary <iulizdz@yahoo.com>
2015-12-16 22:56:45 +0000

    got first pizza

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
   $(document).ready(function() {
     $.get("https://api.foursquare.com/v2/venues/search" +
-        "?client_id=" + client_id + "JLO05W1A52HZ102KB5KDHK2GOPJGICSP1OBHQHDTUKVH0JTK"
-        "&client_secret=" + client_secret + "ZB4PYWK0LDEZETUOG2EC3FV3VLTLNSYX4HAFWP3FPTMFFI01"
+        "?client_id="  + "JLO05W1A52HZ102KB5KDHK2GOPJGICSP1OBHQHDTUKVH0JTK" +
+        "&client_secret=" + "ZB4PYWK0LDEZETUOG2EC3FV3VLTLNSYX4HAFWP3FPTMFFI01" +
         "&v=20151209" +
-        "&near=nyc"
+        "&near=nyc" +
         "&query=pizza",
         function(data) {
             // Callback code goes here. This gets executed after receiving data from the Foursquare API.
+        
+          console.log(data["response"]["venues"]);
+          
         });
+        
   });
+  
